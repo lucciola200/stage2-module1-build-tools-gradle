0ed59bcaa7d7fdc55094b8a0a0b782c4d3c67657
lucciolla200 <teoteo.odisharia@gmail.com>
2023-02-01 07:47:01 +0400

    task-1 v02

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,10 +1,17 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
-        Utils.isAllPositiveNumbers("12", "79");
+        for (String str : args) {
+            if (!StringUtils.isPositiveNumber(str)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,11 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        return StringUtils.isNumeric(str) && Integer.parseInt(str) > 0;
+        try {
+            int number = Integer.parseInt(str);
+            return number > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
